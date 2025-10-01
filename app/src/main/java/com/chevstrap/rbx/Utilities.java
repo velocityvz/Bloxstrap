4d485643c1b8212bf38883c251b3df998099d52d
velocityvz <w4vezq@gmail.com>
2025-10-01 11:22:31 -0500

    Update Utilities.java

--- app/src/main/java/com/chevstrap/rbx/Utilities.java
+++ app/src/main/java/com/chevstrap/rbx/Utilities.java
@@ -1,15 +1,16 @@
-package com.chevstrap.rbx;
+package com.bloxstrap.client;
 
 import android.util.Log;
 
 public class Utilities {
     private static final String TAG = "Utilities";
 
+    // just a simple little function to run shell commands
     public static Process ShellExecute(String[] cmd) {
         try {
             return Runtime.getRuntime().exec(cmd);
         } catch (Exception e) {
-            Log.e(TAG, "Shell execution failed", e);
+            Log.e(TAG, "oops, shell command failed", e);
             return null;
         }
     }
